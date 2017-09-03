36fe5a64f2af7ae4fc77fb228a903e54d34de741
Dan Vyenielo <dvyenielo@gmail.com>
2017-09-03 14:45:39 -0700

    Commentary and clean up.

--- BS_PPP_4-4_statements_try_this_convert_money/BS_PPP_4-4_statements_try_this_convert_money/BSPPP44statementstrythisconvertmoney.cpp
+++ BS_PPP_4-4_statements_try_this_convert_money/BS_PPP_4-4_statements_try_this_convert_money/BSPPP44statementstrythisconvertmoney.cpp
@@ -1,5 +1,12 @@
-// BSPPP44statementstrythisconvertmoney.cpp : Defines the entry point for the console application.
-//
+/*Inspiration for this program from Bjarne Stroustrup's Programming Principles 
+and Practice pg. 105. This program explores the usage of if() statements in 
+C++ via fictional currency conversion.
+I had difficulty converting string data to all lower case for verifying user 
+input validity. process_string_to_lower() utilizes code from stackexchange.
+At this time ::tolower used within transform() is unclear syntax. 
+Author: Dan Vyenielo
+Date: 9/3/2017
+*/
 
 #include "stdafx.h"
 #include <algorithm>
@@ -7,18 +14,26 @@
 #include <string>
 using namespace std;
 
+void prompt_user(double &value, string &unit_in, string &unit_out);
 bool is_value_valid(const double value);
 bool is_unit_valid(const string unit);
 void process_string_to_lower(string &in);
+double currency_conversion(const double &value, const string &unit_in, 
+	const string &unit_out);
+void print_conversion_reciept(const double &value, const double &yield,
+	const string &unit_in, const string &unit_out);
+
 
 int main()
 {
 	double value = -1;
 	string unit_in = "";
 	string unit_out = "";
+	double yield = -1;
 	
-	prompt_user(value, unit_in);
-	currency_conversion(value, unit_in, unit_out);
+	prompt_user(value, unit_in, unit_out);
+	yield = currency_conversion(value, unit_in, unit_out);
+	print_conversion_reciept(value, yield, unit_in, unit_out);
     return 0;
 }
 
@@ -49,49 +64,6 @@ void prompt_user(double &value, string &unit_in, string &unit_out) {
 	}
 }
 
-void currency_conversion(double &value, string &unit_in, string &unit_out) {
-	constexpr double yen_to_usd = 123;
-	constexpr double usd_to_yen = 321;
-	constexpr double yen_to_euro = 100;
-	constexpr double euro_to_yen = 50;
-	constexpr double euro_to_usd = 123;
-	constexpr double usd_to_euro = 75;
-
-	if (unit_in == "euro")
-	{
-		if (unit_out == "usd")
-		{
-
-		}
-		if (unit_out == "yen")
-		{
-
-		}
-	}
-	if (unit_in == "usd")
-	{
-		if (unit_out == "euro")
-		{
-
-		}
-		if (unit_out == "yen")
-		{
-
-		}
-	}
-	if (unit_in == "yen")
-	{
-		if (unit_out == "euro")
-		{
-
-		}
-		if (unit_out == "usd")
-		{
-
-		}
-	}
-}
-
 bool is_value_valid(const double value) {
 	return(value > 0);
 }
@@ -100,6 +72,51 @@ bool is_unit_valid(const string unit) {
 	return ((unit == "euro") || (unit == "usd") || (unit == "yen"));
 }
 
+
 void process_string_to_lower(string &data) {
+	//poached from:https://stackoverflow.com/questions/313970/how-to-convert-stdstring-to-lower-case/24063783#24063783
 	transform(data.begin(), data.end(), data.begin(), ::tolower);
+}
+
+double currency_conversion(const double &value, const string &unit_in, 
+	const string &unit_out) {
+	//fictional conversion values
+	constexpr double euro_to_usd = 123;
+	constexpr double euro_to_yen = 50;
+	constexpr double usd_to_euro = 75;
+	constexpr double usd_to_yen = 321;
+	constexpr double yen_to_euro = 100;
+	constexpr double yen_to_usd = 123;
+
+	double yield;
+	if (unit_in == "euro")
+	{
+		if (unit_out == "usd")
+			yield = value * euro_to_usd;
+		if (unit_out == "yen")
+			yield = value * euro_to_yen;
+	}
+	else if (unit_in == "usd")
+	{
+		if (unit_out == "euro")
+			yield = value * usd_to_euro;
+		if (unit_out == "yen")
+			yield = value * usd_to_yen;
+	}
+	else if (unit_in == "yen")
+	{
+		if (unit_out == "euro")
+			yield = value * yen_to_euro;
+		if (unit_out == "usd")
+			yield = value * yen_to_usd;
+	}
+	else
+		cout << "Error during currency conversion." << endl;
+	return yield;
+}
+
+void print_conversion_reciept(const double &value,const double &yield,
+	const string &unit_in,const string &unit_out) {
+	cout << "Conversion successful." << endl;
+	cout << value << ' ' << unit_in << " converted to " << yield << ' ' << unit_out << endl;
 }
