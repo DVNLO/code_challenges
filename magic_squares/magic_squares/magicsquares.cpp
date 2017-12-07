8ad0a17c7af994dd246ee5a815f5ec4edc3f8a8b
DVNLO <31555490+DVNLO@users.noreply.github.com>
2017-12-07 11:22:17 -0800

    Comparisons are only valid between two values. The comparison returns a 1 or 0 value, so any subsequent comparisons will compare with returned 1. Ahhhh!

--- magic_squares/magic_squares/magicsquares.cpp
+++ magic_squares/magic_squares/magicsquares.cpp
@@ -24,31 +24,96 @@ using namespace std;
 //
 bool check_if_magic(const std::string& square)
 {
-	return (square[4] == 5);
+	if (square[4] != '5')
+		return false;
 	
+	cout << "Begin check " << square << endl;
+	cout << "Center is 5" << endl;
+	cout << "Sum row 1: " << (uint_fast64_t(square[0]) + uint_fast64_t(square[1]) + uint_fast64_t(square[2])) << endl;
+	cout << "Sum row 2: " << (uint_fast64_t(square[3]) + uint_fast64_t(square[4]) + uint_fast64_t(square[5])) << endl;
+	cout << "Sum row 3: " << (uint_fast64_t(square[6]) + uint_fast64_t(square[7]) + uint_fast64_t(square[8])) << endl;
+	cout << "Sum column 1: " << (uint_fast64_t(square[0]) + uint_fast64_t(square[3]) + uint_fast64_t(square[6])) << endl;
+	cout << "Sum column 2: " << (uint_fast64_t(square[1]) + uint_fast64_t(square[4]) + uint_fast64_t(square[7])) << endl;
+	cout << "Sum column 3: " << (uint_fast64_t(square[2]) + uint_fast64_t(square[5]) + uint_fast64_t(square[8])) << endl;
+	cout << "Sum diagonal forward: " << (uint_fast64_t(square[0]) + uint_fast64_t(square[4]) + uint_fast64_t(square[8])) << endl;
+	cout << "Sum diagonal backward: " << (uint_fast64_t(square[2]) + uint_fast64_t(square[4]) + uint_fast64_t(square[6])) << endl;
+
+	if (
+		(int(square[0]) + int(square[1]) + int(square[2])) ==
+		(int(square[3]) + int(square[4]) + int(square[5])) ==
+		(int(square[6]) + int(square[7]) + int(square[8]))
+		)
+		cout << "Sum of rows are equal" << endl;
+	cout << (int(square[0]) + int(square[1]) + int(square[2])) << ' ' << (int(square[3]) + int(square[4]) + int(square[5])) << ' ' << (int(square[6]) + int(square[7]) + int(square[8])) << endl;
+	if (
+		(int(square[0]) + int(square[1]) + int(square[2])) ==
+		(int(square[3]) + int(square[4]) + int(square[5]))
+		)
+		cout << "Two rows are equal" << endl;
+	if (
+		(uint_fast64_t(square[0]) + uint_fast64_t(square[3]) + uint_fast64_t(square[6])) ==
+		(uint_fast64_t(square[1]) + uint_fast64_t(square[4]) + uint_fast64_t(square[7])) ==
+		(uint_fast64_t(square[2]) + uint_fast64_t(square[5]) + uint_fast64_t(square[8]))
+		)
+		cout << "Sum of columns are equal" << endl;
+
+	if (
+		(uint_fast64_t(square[0]) + uint_fast64_t(square[4]) + uint_fast64_t(square[8])) ==
+		(uint_fast64_t(square[2]) + uint_fast64_t(square[4]) + uint_fast64_t(square[6]))
+		)
+		cout << "Sum of diagonals are equal" << endl;
+	
+	if (
+		//sum of rows
+		(
+		(uint_fast64_t(square[0]) + uint_fast64_t(square[1]) + uint_fast64_t(square[2])) ==
+			(uint_fast64_t(square[3]) + uint_fast64_t(square[4]) + uint_fast64_t(square[5])) ==
+			(uint_fast64_t(square[6]) + uint_fast64_t(square[7]) + uint_fast64_t(square[8]))
+			) &&
+		//sum of columns
+			(
+		(uint_fast64_t(square[0]) + uint_fast64_t(square[3]) + uint_fast64_t(square[6])) ==
+				(uint_fast64_t(square[1]) + uint_fast64_t(square[4]) + uint_fast64_t(square[7])) ==
+				(uint_fast64_t(square[2]) + uint_fast64_t(square[5]) + uint_fast64_t(square[8]))
+				) &&
+		//diagonals
+				(
+		(uint_fast64_t(square[0]) + uint_fast64_t(square[4]) + uint_fast64_t(square[8])) ==
+					(uint_fast64_t(square[2]) + uint_fast64_t(square[4]) + uint_fast64_t(square[6]))
+					)
+)
+		cout << "magic square" << endl;
+
 	return (
 		//sum of rows
-		((square[0] + square[1] + square[2]) ==
-			(square[3] + square[4] + square[5]) ==
-			(square[6] + square[7] + square[8])) &&
+		(
+			(uint_fast64_t(square[0]) + uint_fast64_t(square[1]) + uint_fast64_t(square[2])) ==
+			(uint_fast64_t(square[3]) + uint_fast64_t(square[4]) + uint_fast64_t(square[5])) ==
+			(uint_fast64_t(square[6]) + uint_fast64_t(square[7]) + uint_fast64_t(square[8]))
+		) &&
 		//sum of columns
-		((square[0] + square[3] + square[6]) ==
-			(square[1] + square[4] + square[7]) ==
-			(square[2] + square[5] + square[8])) &&
+		(
+			(uint_fast64_t(square[0]) + uint_fast64_t(square[3]) + uint_fast64_t(square[6])) ==
+			(uint_fast64_t(square[1]) + uint_fast64_t(square[4]) + uint_fast64_t(square[7])) ==
+			(uint_fast64_t(square[2]) + uint_fast64_t(square[5]) + uint_fast64_t(square[8]))
+		) &&
 		//diagonals
-		((square[0] + square[4] + square[8]) ==
-			(square[2] + square[4] + square[6]))
-		);
+		(
+			(uint_fast64_t(square[0]) + uint_fast64_t(square[4]) + uint_fast64_t(square[8])) ==
+			(uint_fast64_t(square[2]) + uint_fast64_t(square[4]) + uint_fast64_t(square[6]))
+		)
+	);
 }
 
 // this generates all possible combinations 
 // of 1-9 digits that may or may not 
 // form a magic square
 static string buffer = "000000000";
+static string magic_string = "816357492";
 void generate_or_check(int index_or_check = 8)
 {
 	if (index_or_check == -1) {
-		if (check_if_magic(buffer))
+		if (check_if_magic(magic_string))
 			cout << buffer << " ";
 		return;
 	}
