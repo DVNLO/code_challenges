151fc14ca244eced96dd8e0d9fec9ce82a3bb333
Dan Vyenielo <dvyenielo@gmail.com>
2019-11-13 22:00:27 -0800

    Almost done!

---
+++ hacker_rank_array_manipulation/solution.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the arrayManipulation function below.
+long arrayManipulation(int n, vector<vector<int>> queries) 
+{
+    // sorting the queries before you get them will enable more effective chaceing!
+    vector<long long> vals(n, 0);
+    size_t const A_IDX{ 0 };
+    size_t const B_IDX{ 1 };
+    size_t const K_IDX{ 2 };
+    long long max{ numeric_limits<long long>::min() };
+    for(auto const query : queries)
+    {
+        for(int j{ query[A_IDX] - 1 }; 
+            j < query[B_IDX]; 
+            ++j)
+        {
+            vals[j] += query[K_IDX];
+            max = std::max(max, vals[j]);
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nm_temp;
+    getline(cin, nm_temp);
+
+    vector<string> nm = split_string(nm_temp);
+
+    int n = stoi(nm[0]);
+
+    int m = stoi(nm[1]);
+
+    vector<vector<int>> queries(m);
+    for (int i = 0; i < m; i++) {
+        queries[i].resize(3);
+
+        for (int j = 0; j < 3; j++) {
+            cin >> queries[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    long result = arrayManipulation(n, queries);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
