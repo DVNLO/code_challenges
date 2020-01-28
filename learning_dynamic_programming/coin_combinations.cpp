c9406a0505f0a8212dcb24e4d2f039326140e38a
Daniel Vyenielo <dvyenielo@gmail.com>
2020-01-28 09:35:45 -0800

    Add notes.

--- learning_dynamic_programming/coin_combinations.cpp
+++ learning_dynamic_programming/coin_combinations.cpp
@@ -16,12 +16,16 @@ void print(int const * const arr, int const size)
 	}
 }
 
+// Returns the number of ways to make change with an array
+// of unique coins, for an amount. So, it answers the question,
+// how many ways are there to make change for amt.
+// O(coin_count * amt)
 int change_combinations(int const coins[], 
 			int const coin_count, 
 			int const amt)
 {
-	int const AMT_TERMINAL_IDX{ amt + 1 };
-	int * combinations = new int[AMT_TERMINAL_IDX]{ 0 };
+	int const AMT_TERMINAL_IDX{ amt + 1 };	// 0, 1, 2, 3, ... , amt;
+	int * combinations = new int[AMT_TERMINAL_IDX]{ 0 };	// we only keep a single row from the table. 
 	if(!combinations)
 		return -1;
 	combinations[0] = 1;
@@ -29,7 +33,9 @@ int change_combinations(int const coins[],
 	{
 		for(int j{ coins[i] }; j < AMT_TERMINAL_IDX; ++j)
 		{
-			combinations[j] += combinations[j - coins[i]];
+			combinations[j] += combinations[j - coins[i]];	// so we have the j'th coin
+									// and the number of combinations for the j'th coin
+									// will be equal to 
 		}
 		print(combinations, AMT_TERMINAL_IDX);
 		cout << '\n';
