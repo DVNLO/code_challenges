230f7b4a3e747d8355769b92272bf6f7b1e40a3f
Dan Vyenielo <dvyenielo@gmail.com>
2020-08-16 16:10:44 -0700

    post review optimizaition and learning how to use bitset

--- ctci_1_1_is_unique/main.cpp
+++ ctci_1_1_is_unique/main.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 
-bool is_unique(std::string const & s)
+// while reviewing this problem the bool provided some influential
+// insights for improving my solution. First, you need to clarify
+// what the width of the symbols in the string is. Are they 
+// ASCII or Unicode symbols? Recall that unicode symbols are 16 bits
+// wide therefore they have 2^16 == 65536 permutations. Whereas
+// extended ASCII is only 2^8 == 256 symbols. 
+
+// Second, since we are only checking if s is unique, we can use a 
+// boolean array for the counts. 
+
+// third, we can take this a step further and use a bitset.
+// https://www.youtube.com/watch?v=jqJ5s077OKo 
+
+// fourth, if the length of the string is more than the number of
+// symbols in the language their must be a duplicate in the string.
+
+bool is_unique_og(std::string const & s)
 {
     bool ret{ true };
     size_t const UNIQUE_SYMBOL_COUNT{ 256U };
@@ -26,6 +42,36 @@ bool is_unique(std::string const & s)
     return ret;
 }
 
+// the following is an implementation following solution review leveraging
+// the helpful edge cases and using a more memory effecient datastructure
+// the bitset. 
+
+bool is_unique(std::string const & s)
+{
+    size_t constexpr UNIQUE_SYMBOL_COUNT{ 256U };
+    size_t const SIZE_S{ s.size() };
+    if(SIZE_S > UNIQUE_SYMBOL_COUNT)
+    {
+        return false;
+    }
+    std::bitset<UNIQUE_SYMBOL_COUNT> counts;    // initialized to 0 by default
+    size_t cur_symbol;
+    for(size_t i{ 0U }; i < SIZE_S; ++i)
+    {
+        cur_symbol = static_cast<size_t>(s[i]);
+        if(counts.test(cur_symbol)) // -> cur_symbol is a duplicate 
+        {
+            return false;
+        }
+        else
+        {
+            counts.set(cur_symbol);
+        }
+    }
+    return true;
+}
+
+
 int main()
 {
     std::string s;
@@ -40,4 +86,3 @@ int main()
     }
     std::cout << std::endl;
 }
-
