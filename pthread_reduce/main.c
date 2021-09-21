fbdf38b3d45f147bedfe87d258d1f790ba48b94f
Dan Vyenielo <dvyenielo@protonmail.com>
2021-09-21 13:04:24 -0700

    neat

--- pthread_reduce/main.c
+++ pthread_reduce/main.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
+#include <limits.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 typedef struct
 {
     int * vals;
     size_t n;
-    int sum;
+    long long sum;
 } 
-arr;
+arr;    //gument pack
 
-void * sum(void * arg)
+void * sum(void * const arg)
 {
-    arr * vals = (arr *)(arg);
-    int s = 0;
+    arr * const vals = (arr *)(arg);
+    long long s = 0;
     for(size_t i = 0; i < vals->n; ++i)
     {
         s += vals->vals[i];
@@ -24,8 +25,10 @@ void * sum(void * arg)
 
 int main()
 {
-    size_t const size_vals = 1008;
-    int * vals = (int *)(malloc(sizeof(int) * size_vals));
+    int const thread_count = 24;
+    size_t const size_vals = INT_MAX - INT_MAX % 24;
+    printf("size_vals == %lu\n", size_vals);
+    int * const vals = (int *)(malloc(sizeof(int) * size_vals));
     if(!vals)
     {
         return EXIT_FAILURE;
@@ -34,8 +37,7 @@ int main()
     {
         vals[i] = i + 1;
     }
-    int thread_count = 24;
-    pthread_t * thread_ids = (pthread_t *)(malloc(sizeof(pthread_t) * thread_count));
+    pthread_t * const thread_ids = (pthread_t *)(malloc(sizeof(pthread_t) * thread_count));
     if(!thread_ids)
     {
         return EXIT_FAILURE;
@@ -43,28 +45,30 @@ int main()
     int rc;
     for(int i = 0; i < thread_count; ++i)
     {
-        arr * thread_data = (arr *)(malloc(sizeof(arr)));
+        arr * const thread_data = (arr *)(malloc(sizeof(arr)));
         if(!thread_data)
         {
             return EXIT_FAILURE;
         }
-        thread_data->n = size_vals/thread_count;
-        thread_data->vals = vals + (i * thread_data->n);    // TODO : handle remainder case
+        //thread_data->n = size_vals/thread_count;
+        thread_data->n = size_vals;
+        //thread_data->vals = vals + (i * thread_data->n);    // TODO : handle remainder case
+        thread_data->vals = vals;
         rc = pthread_create(&thread_ids[i], NULL, sum, thread_data);
         if(rc)
         {
             return EXIT_FAILURE;
         }
     }
-    int sum = 0;
+    long long sum = 0;
     void * ret;
     for(int i = 0; i < thread_count; ++i)
     {
         rc = pthread_join(thread_ids[i], &ret);
-        arr * thread_data = (arr *)(ret);
-        sum += thread_data->sum;
+        arr * const thread_data = (arr *)(ret);
+        sum += thread_data->sum;    // overflows when all do equal work, duh
     }
-    printf("%i\n", sum);
+    printf("%lli\n", sum);
     free(thread_ids);
     free(vals);
     return EXIT_SUCCESS;
