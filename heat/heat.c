dfc8969a9e7401fef8d34d0b82a46614fb95765c
Dan Vyenielo <dvyenielo@protonmail.com>
2022-02-23 12:01:09 -0800

    Clean heat.

--- heat/heat.c
+++ heat/heat.c
@@ -3,8 +3,6 @@ The following program produces heat for my room while I wait until I have time
 to purchase a space heater. 
 Author: Dan Vyenielo (dvyenielo@gmail.com)
 */
-#include <stdio.h>
-
 int main()
 {
     volatile int a;
@@ -13,13 +11,3 @@ int main()
 	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
